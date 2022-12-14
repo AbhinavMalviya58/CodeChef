c212877e7b802e05bdabee924961b9c6af2417c1
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2022-12-14 23:37:38 +0530

    14th DEC UPDATE

--- Codechef_14_Dec_1.java
+++ Codechef_14_Dec_1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Codechef_14_Dec_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int t = sc.nextInt();
         while(t-- != 0){
             int n = sc.nextInt();
--- Codechef_14_Dec_2.java
+++ Codechef_14_Dec_2.java
@@ -4,6 +4,7 @@ public class Codechef_14_Dec_2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+        
         while(t-- != 0){
         int A = sc.nextInt();
         int B = sc.nextInt();
